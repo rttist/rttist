cf188895f31030ec7e5cd262c5c2f6e95395a4cb
Hookyns <hookcze@gmail.com>
2024-09-08 22:35:01 +0200

    WIP repo changes

--- packages/plugins/vite-plugin-rttist/src/index.ts
+++ packages/plugins/vite-plugin-rttist/src/index.ts
@@ -1,4 +1,4 @@
-import type { ConfigEnv, Plugin, ResolvedConfig, UserConfig } from "vite";
+import { ConfigEnv, ModuleNode, Plugin, ResolvedConfig, UserConfig } from "vite";
 import * as fs from "node:fs";
 import { resolve } from "node:path";
 import { PackageInfo, transform } from "@rttist/ts-loader-wasm";
@@ -68,78 +68,115 @@ export function rttistPlugin(pluginOptions: RttistPluginOptions): Plugin {
 				return "\0" + id;
 			}
 		},
+		configureServer(server) {
+			const [metadataUrl, metadataModuleNode] =
+				Array.from(server.moduleGraph.urlToModuleMap.entries()).find(([url, module]) =>
+					url.endsWith("/metadata.typelib.ts")
+				) ?? [];
+			// server.moduleGraph.getModuleByUrl(met);
+			// server.moduleGraph.updateModuleInfo(metadataModuleNode);
+			console.log(metadataUrl, metadataModuleNode);
+		},
+
 		handleHotUpdate(ctx) {
-			// Ignore update of metadata.typelib.ts; it's changed together with internal.typelib that causes a loop.
-			// Ignoring metadata.typelib is fine, because it gets invalidated by update of internal.typelib.ts.
-			if (ctx.file.includes("/metadata.typelib.ts")) {
-				// metadataTypelibModule = ctx.modules[0];
-
-				// for (let module of ctx.modules) {
-				// 	for (let importer of module.importers) {
-				// 		ctx.server.ws.send({
-				// 			type: "update",
-				// 			updates: [
-				// 				{
-				// 					type: `js-update`,
-				// 					timestamp: ctx.timestamp,
-				// 					path: importer.url,
-				// 					explicitImportRequired: false,
-				// 					// explicitImportRequired: boundary.type === 'js'
-				// 					// 	? isExplicitImportRequired(acceptedVia.url)
-				// 					// 	: undefined,
-				// 					acceptedPath: importer.url,
-				// 				},
-				// 			],
-				// 		});
-				// 	}
-				// }
-
-				return [];
-			}
-
-			// Ignore update of depending modules when internal.typelib is updated
-			if (ctx.file.includes("/internal.typelib.js")) {
-				// // Also use `server.ws.send` to support Vite <5.1 if needed
-				// ctx.server.hot.send({ type: 'full-reload' });
-
-				// if (metadataTypelibModule) {
-				// 	ctx.server.ws.send({
-				// 		type: "update",
-				// 		updates: [
-				// 			{
-				// 				type: `js-update`,
-				// 				timestamp: ctx.timestamp,
-				// 				path: metadataTypelibModule.url,
-				// 				explicitImportRequired: true,
-				// 				// explicitImportRequired: boundary.type === 'js'
-				// 				// 	? isExplicitImportRequired(acceptedVia.url)
-				// 				// 	: undefined,
-				// 				acceptedPath: metadataTypelibModule.url,
-				// 			},
-				// 		],
-				// 	});
-				// }
-
-				// // Invalidate modules manually
-				// const invalidatedModules = new Set<ModuleNode>();
-				// for (const mod of ctx.modules) {
-				// 	ctx.server.moduleGraph.invalidateModule(mod, invalidatedModules, ctx.timestamp, true);
-				// }
-
-				return [];
-			}
+			// const invalidatedModules = new Set<ModuleNode>();
+			//
+			// for (let module of ctx.modules) {
+			// 	for (let importer of module.importers) {
+			// 		if (importer.url.includes("/metadata.typelib.ts")) {
+			// 			continue;
+			// 		}
+			// 		ctx.server.moduleGraph.invalidateModule(importer, invalidatedModules, ctx.timestamp, true);
+			// 		// ctx.server.hot.send({
+			// 		// 	type: "update",
+			// 		// 	updates: [
+			// 		// 		{
+			// 		// 			type: `js-update`,
+			// 		// 			timestamp: ctx.timestamp,
+			// 		// 			path: importer.url,
+			// 		// 			explicitImportRequired: false,
+			// 		// 			// explicitImportRequired: boundary.type === 'js'
+			// 		// 			// 	? isExplicitImportRequired(acceptedVia.url)
+			// 		// 			// 	: undefined,
+			// 		// 			acceptedPath: importer.url,
+			// 		// 		},
+			// 		// 	],
+			// 		// });
+			// 	}
+			// }
+			//
+			// // Ignore update of metadata.typelib.ts; it's changed together with internal.typelib that causes a loop.
+			// // Ignoring metadata.typelib is fine, because it gets invalidated by update of internal.typelib.ts.
+			// if (ctx.file.includes("/metadata.typelib.ts")) {
+			// 	// metadataTypelibModule = ctx.modules[0];
+			//
+			// 	// for (let module of ctx.modules) {
+			// 	// 	for (let importer of module.importers) {
+			// 	// 		ctx.server.ws.send({
+			// 	// 			type: "update",
+			// 	// 			updates: [
+			// 	// 				{
+			// 	// 					type: `js-update`,
+			// 	// 					timestamp: ctx.timestamp,
+			// 	// 					path: importer.url,
+			// 	// 					explicitImportRequired: false,
+			// 	// 					// explicitImportRequired: boundary.type === 'js'
+			// 	// 					// 	? isExplicitImportRequired(acceptedVia.url)
+			// 	// 					// 	: undefined,
+			// 	// 					acceptedPath: importer.url,
+			// 	// 				},
+			// 	// 			],
+			// 	// 		});
+			// 	// 	}
+			// 	// }
+			//
+			// 	return [];
+			// }
+			//
+			// // Ignore update of depending modules when internal.typelib is updated
+			// if (ctx.file.includes("/internal.typelib.js")) {
+			// 	// // Also use `server.ws.send` to support Vite <5.1 if needed
+			// 	// ctx.server.hot.send({ type: 'full-reload' });
+			//
+			// 	// if (metadataTypelibModule) {
+			// 	// 	ctx.server.ws.send({
+			// 	// 		type: "update",
+			// 	// 		updates: [
+			// 	// 			{
+			// 	// 				type: `js-update`,
+			// 	// 				timestamp: ctx.timestamp,
+			// 	// 				path: metadataTypelibModule.url,
+			// 	// 				explicitImportRequired: true,
+			// 	// 				// explicitImportRequired: boundary.type === 'js'
+			// 	// 				// 	? isExplicitImportRequired(acceptedVia.url)
+			// 	// 				// 	: undefined,
+			// 	// 				acceptedPath: metadataTypelibModule.url,
+			// 	// 			},
+			// 	// 		],
+			// 	// 	});
+			// 	// }
+			//
+			// 	// // Invalidate modules manually
+			// 	// const invalidatedModules = new Set<ModuleNode>();
+			// 	// for (const mod of ctx.modules) {
+			// 	// 	ctx.server.moduleGraph.invalidateModule(mod, invalidatedModules, ctx.timestamp, true);
+			// 	// }
+			//
+			// 	return [];
+			// }
 		},
 		async load(id: string) {
-			// TODO: This is way how to handle HMR of metadata.typelib; we can rewrite types in global type lib
-			// if (id.endsWith("/metadata.typelib.ts")) {
-			// 	return (
-			// 		(await fs.promises.readFile(id, "utf8")) +
-			// 		`
+			// 			// TODO: This is way how to handle HMR of metadata.typelib; we can rewrite types in global type lib
+			// 			if (viteCommand === "serve" && id.endsWith("/metadata.typelib.ts")) {
+			// 				return (
+			// 					(await fs.promises.readFile(id, "utf8")) +
+			// 					`
 			// if (import.meta.hot) {
-			// 	import.meta.hot.on("vite:beforeUpdate", () => {
-			// 		Metadata.clearMetadata();
-			// 	});
-			// 	import.meta.hot.accept();
+			// 	import.meta.hot.accept(["rttist", "./internal.typelib"]);
+			// 	// import.meta.hot.on("vite:beforeUpdate", () => {
+			// 	// 	Metadata.clearMetadata();
+			// 	// });
+			// 	// import.meta.hot.accept();
 			// }`
 			// 				);
 			// 			}
